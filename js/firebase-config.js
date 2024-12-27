2f42af85647749c2758c1787f0c1118c81e16968
rafaelFaleiros <faelfaleiros@gmail.com>
2024-12-27 18:33:21 -0300

    puxando credenciais atualizadas

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBPYWkjI02UlNkDstXZ4oeIs2sL1WG0AAg",
-  authDomain: "meu-painel-alura.firebaseapp.com",
-  projectId: "meu-painel-alura",
-  storageBucket: "meu-painel-alura.appspot.com",
-  messagingSenderId: "854143721967",
-  appId: "1:854143721967:web:7bd6d72db840648c9c161a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
+  
 // Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
 
